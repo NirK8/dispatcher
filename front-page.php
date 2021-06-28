83a33d9b27090cbcc469ff31d5a4d3a0b1723b2a
Nir Kahana <>
2021-06-28 10:03:09 +0300

    more general button component

--- front-page.php
+++ front-page.php
@@ -1,7 +1,8 @@
 <?php get_header();?>
 <?php
 get_template_part('partials/tag', null, array('crypto'));
-get_template_part('partials/button', null, array('navigate to dispatch', 'arrow'));
-
+get_template_part('partials/button', null, array('navigate to dispatch', 'inline-button', true));
+?>
+<?php
 ?>
 <?php get_footer(); ?>
